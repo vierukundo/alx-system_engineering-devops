ce3b5c8e0183a8205e9506d4b2dc6f588c0e3cd7
vierukundo <vierukundo@gmail.com>
2023-10-23 14:22:00 -0700

    script to export data in the JSON format

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3i
+"""Needed module to retrieve dat from api"""
+import json
+import requests
+import sys
+
+
+def get_employee_todo_progress(employee_id):
+    """Define the base URL for the API"""
+
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    params = {'userId': employee_id}
+
+    # Make a GET request to retrieve the user's information
+    user_response = requests.get(f"{base_url}/users/{employee_id}")
+    user_data = user_response.json()
+    employee_username = user_data.get('username')
+
+    # Make a GET request to retrieve the user's TODO list with the parameters
+    todo_response = requests.get(f"{base_url}/todos", params=params)
+
+    todo_list = todo_response.json()
+    # export data in the CSV format
+    with open("{}.json".format(employee_id), 'w') as f:
+        task_list = []
+        user_associated_task = {}
+
+        for task in todo_list:
+            task_info = {
+                    "task": task.get('title'),
+                    "completed": task.get('completed'),
+                    "username": employee_username
+                    }
+            task_list.append(task_info)
+        user_associated_task["{}".format(employee_id)] = task_list
+        serialized_data = json.dumps(user_associated_task)
+        f.write(serialized_data)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+    else:
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
