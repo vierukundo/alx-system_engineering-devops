97bd62fb299f1af1ca15dff596800949bba5d56c
vierukundo <vierukundo@gmail.com>
2023-11-16 22:59:38 +0000

    increase ULIMIT

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,11 @@
-exec {'Raise_the_ULIMIT':
-  command  => 'sed -i "s/15/10000/" /etc/default/nginx',
-  provider => 'shell',
+# 0-the_sky_is_the_limit_not.pp
+
+exec {'raise_the_ULIMIT':
+  command => 'sed -i "s|ULIMIT=\"-n 15\"|ULIMIT=\"-n 10000\"|" /etc/default/nginx',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
 
-exec {'restart nginx':
-  command  => 'service nginx restart',
-  provider => 'shell',
+exec {'restart_nginx':
+  command => 'service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
