cd08c62ae1aa7fdbb5ae1afb29560afe870e19ae
vierukundo <103329481+vierukundo@users.noreply.github.com>
2023-11-16 20:35:40 +0200

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 exec {'Raise the ULIMIT':
-  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  command => '/bin/sed -i "s/15/10000/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
 
